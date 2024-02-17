535ea1893888e621f51ef9fcf1830717dc6eb730
William Li <52115161+Trollermaner@users.noreply.github.com>
2024-02-17 06:53:15 -0500

    add gitignore file

--- backend/services/group.service.js
+++ backend/services/group.service.js
@@ -1,13 +1,29 @@
 const Group = require('../models/group.model')
 
-const createGroup = (body) => {
-    
-    //add author to group automatically
-    
+const createGroup = async (body) => {
+    const author = body.users //include the author and everyone they're adding
+    const groupName = body.name
 
+    if (!(author && groupName)) {
+        throw new Error("Incorect Input")
+    }
+    
+    const newGroup = new Group()
+    newGroup.name = groupName
+    newGroup.users = [author]
+
+    return await newGroup.save()
 }
 
-const addToGroup = (users, invite) => {
-    //set user to be an array
-    //search which group by invite code
+const addToGroup = async (body) => {
+    const users = body.users
+    const invite = body.id
+    
+    if (!(users && invite)) {
+        throw new Error("Incorect Input")
+    }
+
+    const group = Group.findById(invite)
+    group.users = [...group.users, ...users]
+    return await group.save()
 }
