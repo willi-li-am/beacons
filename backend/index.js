2f2a4cd254daed9fc8ce25ce017d2874d21161f3
William Li <52115161+Trollermaner@users.noreply.github.com>
2024-02-17 05:12:22 -0500

    init mongodb

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,22 @@
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
 
-app.listen(8080, () => {
-    console.log('Listening on port 8080')
+require('dotenv').config()
+const port = process.env.PORT || 8080
+const uri = process.env.ATLAS_URI
+mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    socketTimeoutMS: 1000 * 20,
+    connectTimeoutMS: 1000 * 20,
+})
+
+const connection = mongoose.connection
+connection.once('open', () => {
+    console.log('MongoDB database connection established successfully')
+})
+
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`)
 })
