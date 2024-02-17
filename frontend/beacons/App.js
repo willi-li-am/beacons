8059213c4a8533584b7a90686a1f4ac8ca81feaa
ritaxiang <zherita0502@gmail.com>
2024-02-17 15:48:55 -0500

    added screens

--- frontend/beacons/App.js
+++ frontend/beacons/App.js
@@ -1,12 +1,56 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createStackNavigator } from '@react-navigation/stack';
+import { Ionicons } from '@expo/vector-icons';
+import LoginScreen from './screens/loginScreen';
+import EventsScreen from './screens/eventsScreen';
+
+const Tab = createBottomTabNavigator();
+const Stack = createStackNavigator();
+
+const BottomTabNavigator = () => {
+
+  return (
+    
+    <Tab.Navigator screenOptions={{ headerShown: false }}>
+      <Tab.Screen
+        name="Events"
+        component={EventsScreen}
+        options={{
+          tabBarIcon: ({ focused, color, size }) => (
+            <Ionicons
+              name={focused ? 'person' : 'person-circle-outline'}
+              size={size}
+              color={'#059905'}
+            />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  )
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+        <Stack.Navigator
+          initialRouteName="login"
+          screenOptions={{
+            headerShown: false,
+            tabBarActiveTintColor: 'tomato',
+            tabBarInactiveTintColor: 'gray',
+          }}
+        >
+          <Stack.Screen
+            name="login"
+            component={LoginScreen}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen name="TabNavigator" component={BottomTabNavigator} />
+        </Stack.Navigator>
+      </NavigationContainer>
   );
 }
 
