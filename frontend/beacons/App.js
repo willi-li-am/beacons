3f424b1f1dfc14e6db44952ada7eb10bbc0aace0
sallyjeong <sallyjeong01@gmail.com>
2024-02-18 05:11:45 -0500

    Update App.js

--- frontend/beacons/App.js
+++ frontend/beacons/App.js
@@ -8,10 +8,10 @@ import { Ionicons } from '@expo/vector-icons';
 // Import your screens
 import LoginScreen from './screens/LoginScreen';
 import MapScreen from './screens/MapScreen';
-import ProfileScreen from './screens/profileScreen';
+import ProfileScreen from './screens/ProfileScreen';
 import SearchScreen from './screens/SearchScreen';
 import FriendsScreen from './screens/FriendsScreen';
-import EventsScreen from './screens/eventsScreen';
+import EventsScreen from './screens/EventsScreen';
 import { AuthContextProvider } from './hooks/AuthContext';
 
 // Define any additional screens you have
