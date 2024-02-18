4def613e178fb94c69c89a771376b587b476c38f
William Li <52115161+Trollermaner@users.noreply.github.com>
2024-02-18 05:49:19 -0500

    backend stuff:

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -3,6 +3,7 @@ const mongoose = require ('mongoose');
 const {Schema} = mongoose
 
 const UserSchema = new Schema({
+    _id: {type: String, required: true},
     email: {type:String , required: true, unique: true},
     name: {type:String, required: true},
     password: {type:String, required: true},
