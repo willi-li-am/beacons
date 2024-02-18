e95483af03665253b2aaa48f1aa4947dce0c2312
Owen Zhang <67778083+owenz0523@users.noreply.github.com>
2024-02-18 06:25:23 -0500

    adada

--- frontend/beacons/screens/SearchScreen.js
+++ frontend/beacons/screens/SearchScreen.js
@@ -1,12 +1,44 @@
 // Import React and necessary components from React Native
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { View, TextInput, Button, Text, Image, FlatList, StyleSheet } from 'react-native';
 
 // Define the component
+const UserCard = ({ user }) => (
+  <View style={styles.card}>
+    <Text style={styles.username}>{user.name}</Text>
+  </View>
+);
+
 const SearchScreen = () => {
+
+  const [query, setQuery] = useState('');
+  const [users, setUsers] = useState([]);
+
+  const searchUsers = async () => {
+    console.log(query);
+    try {
+      const response = await fetch(`https://beacon-9ob2.onrender.com/user/${query}`);
+      setUsers(response.data);
+      console.log(response.data) // assuming the response data is an array of user objects
+    } catch (error) {
+      console.error('Error fetching users:', error);
+    }
+  };
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>This is a blank screen for testing</Text>
+    <View>
+      <TextInput
+        style={styles.input}
+        value={query}
+        onChangeText={setQuery}
+        placeholder="Search users..."
+      />
+      <Button title="Search" onPress={searchUsers} />
+      <FlatList
+        data={users}
+        keyExtractor={item => item.id.toString()} // Replace 'id' with the unique identifier of your user objects
+        renderItem={({ item }) => <UserCard user={item} />}
+      />
     </View>
   );
 };
@@ -17,11 +49,34 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#f0f0f0', // Light grey background
+    backgroundColor: '#DCC7FF',
   },
   text: {
     fontSize: 18,
     color: '#333', // Dark grey text
+  },
+  input: {
+    marginTop: 50,
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+  card: {
+    flexDirection: 'row',
+    padding: 10,
+    alignItems: 'center',
+    borderBottomWidth: 1,
+    borderBottomColor: '#cccccc',
+  },
+  profilePic: {
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+    marginRight: 10,
+  },
+  username: {
+    fontSize: 18,
   },
 });
 
